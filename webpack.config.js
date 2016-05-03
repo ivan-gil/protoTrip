89e2cae869f22566ec9270f1d7cbc2fa8046f62b
Ivan Hil <vanya.gil95@gmail.com>
2016-05-03 21:46:22 +0300

    [+] change webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
-const NODE_ENV = process.env.NODE_ENV || "dev";
-const webpack = require('webpack');
+var NODE_ENV = process.env.NODE_ENV || "dev";
+var webpack = require('webpack');
+var path = require('path');
 module.exports = {
-    entry: "./app/main.js",
+    entry: {
+    	app: ["./app/main.es"],
+    	vendor: [
+    		'react',
+    		'react-router',
+    		'redux',
+    		'react-redux'
+    	]
+    },
     output: {
-        path: './public/build/',
-        filename: "bundle.js",
+        path: path.join(__dirname, '/public/build/'),
+        filename: '[name].bundle.js',
         library: "TRP"
     },
     watch: NODE_ENV === "dev",
@@ -12,19 +21,32 @@ module.exports = {
     	aggregateTimeout: 100
     },
     devtool: NODE_ENV === "dev" ? "cheap-inline-module-source-map" : null,
-
+    module: {
+    	loaders: [
+    		{ test: /\.es$/, loaders: ['babel?presets[]=es2015'] },
+    		{ test: /\.jsx$/, loaders: ['jsx-loader?insertPragma'] },
+    		{ test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
+    		{ test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'scss-loader'] },
+    		{ test: /\.png$/, loaders: ['url-loader?limit=150000'] },
+    		{ test: /\.jpg$/, loaders: ['url-loader?limit=150000'] },
+    		{ test: /\.ttf$/, loaders: ['url-loader?limit=150000'] },
+    		{ test: /\.json$/, loaders: ['json-loader'] },
+    	]
+    },
+    resolve: {
+    	root: [path.join(__dirname, 'app/scripts')],
+    	extensions: ['', '.js', 'es', 'css', 'scss'],
+    	moduleDirectories: ['node_modules']
+    },
     plugins: [
     	new webpack.DefinePlugin({
     		NODE_ENV: JSON.stringify('NODE_ENV')
+    	}),
+    	new webpack.optimize.CommonsChunkPlugin({
+    		name:['vendor'],
+    		minChunks: Infinity
     	})
     ],
-
-    module: {
-    	loaders: [{
-    		test: /\.js$/,
-    		loader: 'babel?presets[]=es2015'
-    	}]
-    }
 };
 
 if(NODE_ENV === 'prod') {
